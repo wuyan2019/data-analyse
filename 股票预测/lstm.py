abb4a566cee0cebd528e0ed5ec4be7a7bc5b393b
wuyan2019 <645301211@qq.com>
2019-05-22 23:19:26 +0800

    lstm

--- 股票预测/lstm.py
+++ 股票预测/lstm.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor as T
 from torch.nn import Parameter as P
-from torch.autograd import Variable as V
+from torch.autograd import Variable
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -16,7 +16,7 @@ from datetime import timedelta
 
 
 sns.set()
-df = pd.read_csv('GOOG-year.csv')
+df = pd.read_csv('./GOOG-year.csv')
 date_ori = pd.to_datetime(df.iloc[:, 0]).tolist()
 df.head()
 
@@ -26,109 +26,89 @@ df_log = pd.DataFrame(df_log)
 df_log.head()
 
 
-class LSTM(nn.Module):
-    """
-    An implementation of Hochreiter & Schmidhuber:
-    'Long-Short Term Memory'
-    http://www.bioinf.jku.at/publications/older/2604.pdf
-    Special args:
-
-    dropout_method: one of
-            * pytorch: default dropout implementation
-            * gal: uses GalLSTM's dropout
-            * moon: uses MoonLSTM's dropout
-            * semeniuta: uses SemeniutaLSTM's dropout
-    """
-
-    def __init__(self, input_size, hidden_size, bias=True, dropout=0.0, dropout_method='pytorch'):
-        super(LSTM, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.bias = bias
-        self.dropout = dropout
-        self.i2h = nn.Linear(input_size, 4 * hidden_size, bias=bias)
-        self.h2h = nn.Linear(hidden_size, 4 * hidden_size, bias=bias)
-        self.reset_parameters()
-        assert (dropout_method.lower() in ['pytorch', 'gal', 'moon', 'semeniuta'])
-        self.dropout_method = dropout_method
-
-    def sample_mask(self):
-        keep = 1.0 - self.dropout
-        self.mask = V(th.bernoulli(T(1, self.hidden_size).fill_(keep)))
-
-    def reset_parameters(self):
-        std = 1.0 / math.sqrt(self.hidden_size)
-        for w in self.parameters():
-            w.data.uniform_(-std, std)
-
-    def forward(self, x, hidden):
-        do_dropout = self.training and self.dropout > 0.0
-        h, c = hidden
-        h = h.view(h.size(1), -1)
-        c = c.view(c.size(1), -1)
-        x = x.view(x.size(1), -1)
-
-        # Linear mappings
-        preact = self.i2h(x) + self.h2h(h)
-
-        # activations
-        gates = preact[:, :3 * self.hidden_size].sigmoid()
-        g_t = preact[:, 3 * self.hidden_size:].tanh()
-        i_t = gates[:, :self.hidden_size]
-        f_t = gates[:, self.hidden_size:2 * self.hidden_size]
-        o_t = gates[:, -self.hidden_size:]
-
-        # cell computations
-        if do_dropout and self.dropout_method == 'semeniuta':
-            g_t = F.dropout(g_t, p=self.dropout, training=self.training)
-
-        c_t = th.mul(c, f_t) + th.mul(i_t, g_t)
-
-        if do_dropout and self.dropout_method == 'moon':
-            c_t.data.set_(th.mul(c_t, self.mask).data)
-            c_t.data *= 1.0 / (1.0 - self.dropout)
-
-        h_t = th.mul(o_t, c_t.tanh())
-
-        # Reshape for compatibility
-        if do_dropout:
-            if self.dropout_method == 'pytorch':
-                F.dropout(h_t, p=self.dropout, training=self.training, inplace=True)
-            if self.dropout_method == 'gal':
-                h_t.data.set_(th.mul(h_t, self.mask).data)
-                h_t.data *= 1.0 / (1.0 - self.dropout)
-
-        h_t = h_t.view(1, h_t.size(0), -1)
-        c_t = c_t.view(1, c_t.size(0), -1)
-        return h_t, (h_t, c_t)
+# class LSTM(nn.Module):
+#     """
+#     An implementation of Hochreiter & Schmidhuber:
+#     'Long-Short Term Memory'
+#     http://www.bioinf.jku.at/publications/older/2604.pdf
+#     Special args:
+#
+#     dropout_method: one of
+#             * pytorch: default dropout implementation
+#             * gal: uses GalLSTM's dropout
+#             * moon: uses MoonLSTM's dropout
+#             * semeniuta: uses SemeniutaLSTM's dropout
+#     """
+#
+#     def __init__(self, input_size, hidden_size, bias=True, dropout=0.0, dropout_method='pytorch'):
+#         super(LSTM, self).__init__()
+#         self.input_size = input_size
+#         self.hidden_size = hidden_size
+#         self.bias = bias
+#         self.dropout = dropout
+#         self.i2h = nn.Linear(input_size, 4 * hidden_size, bias=bias)
+#         self.h2h = nn.Linear(hidden_size, 4 * hidden_size, bias=bias)
+#         self.reset_parameters()
+#         assert (dropout_method.lower() in ['pytorch', 'gal', 'moon', 'semeniuta'])
+#         self.dropout_method = dropout_method
+#
+#     def sample_mask(self):
+#         keep = 1.0 - self.dropout
+#         self.mask = V(th.bernoulli(T(1, self.hidden_size).fill_(keep)))
+#
+#     def reset_parameters(self):
+#         std = 1.0 / math.sqrt(self.hidden_size)
+#         for w in self.parameters():
+#             w.data.uniform_(-std, std)
+#
+#     def forward(self, x, hidden):
+#         do_dropout = self.training and self.dropout > 0.0
+#         h, c = hidden
+#         h = h.view(h.size(1), -1)
+#         c = c.view(c.size(1), -1)
+#         x = x.view(x.size(1), -1)
+#
+#         # Linear mappings
+#         preact = self.i2h(x) + self.h2h(h)
+#
+#         # activations
+#         gates = preact[:, :3 * self.hidden_size].sigmoid()
+#         g_t = preact[:, 3 * self.hidden_size:].tanh()
+#         i_t = gates[:, :self.hidden_size]
+#         f_t = gates[:, self.hidden_size:2 * self.hidden_size]
+#         o_t = gates[:, -self.hidden_size:]
+#
+#         # cell computations
+#         if do_dropout and self.dropout_method == 'semeniuta':
+#             g_t = F.dropout(g_t, p=self.dropout, training=self.training)
+#
+#         c_t = th.mul(c, f_t) + th.mul(i_t, g_t)
+#
+#         if do_dropout and self.dropout_method == 'moon':
+#             c_t.data.set_(th.mul(c_t, self.mask).data)
+#             c_t.data *= 1.0 / (1.0 - self.dropout)
+#
+#         h_t = th.mul(o_t, c_t.tanh())
+#
+#         # Reshape for compatibility
+#         if do_dropout:
+#             if self.dropout_method == 'pytorch':
+#                 F.dropout(h_t, p=self.dropout, training=self.training, inplace=True)
+#             if self.dropout_method == 'gal':
+#                 h_t.data.set_(th.mul(h_t, self.mask).data)
+#                 h_t.data *= 1.0 / (1.0 - self.dropout)
+#
+#         h_t = h_t.view(1, h_t.size(0), -1)
+#         c_t = c_t.view(1, c_t.size(0), -1)
+#         return h_t, (h_t, c_t)
 
 
 class ModelClass(torch.nn.Module):
-    def __init__(self, num_layers, size, size_layer, output_size):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(ModelClass, self).__init__()
-        self.lstm = torch.nn.LSTM(size, size_layer, batch_first=True)
+        self.lstm = torch.nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.drop = torch.nn.Dropout(0.5)
-        self.linear1 = torch.nn.Linear(num_layers * 2 * size_layer, output_size)
-
-    def forward(self, x):
-        x = self.lstm(x)
-        x = self.drop(x)
-        x = self.linear1(x)
-        return x
-
-
-class RNN(nn.Module):
-    def __init__(self):
-        super(RNN, self).__init__()
-
-        self.rnn = nn.LSTM(     # LSTM 效果要比 nn.RNN() 好多了
-            input_size=28,      # 图片每行的数据像素点
-            hidden_size=64,     # rnn hidden unit
-            num_layers=1,       # 有几层 RNN layers
-            batch_first=True,   # input & output 会是以 batch size 为第一维度的特征集 e.g. (batch, time_step, input_size)
-        )
-
-        self.out = nn.Linear(64, 10)    # 输出层
+        self.out = torch.nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         # x shape (batch, time_step, input_size)
@@ -143,23 +123,48 @@ class RNN(nn.Module):
         return out
 
 
+class RNN(nn.Module):
+    def __init__(self):
+        super(RNN, self).__init__()
+
+        self.rnn = nn.LSTM(     # LSTM 效果要比 nn.RNN() 好多了
+            input_size=200,      # 图片每行的数据像素点
+            hidden_size=256,     # rnn hidden unit
+            num_layers=1,       # 有几层 RNN layers
+            batch_first=True,   # input & output 会是以 batch size 为第一维度的特征集 e.g. (batch, time_step, input_size)
+        )
+
+        self.out = nn.Linear(256, 52)    # 输出层
+
+    def forward(self, x, h_state):
+        # x (batch, time_step, input_size)
+        # h_state (n_layers, batch, hidden_size)
+        # r_out (batch, time_step, hidden_size)
+        r_out, h_state = self.rnn(x, h_state)
+
+        outs = []  # save all predictions
+        for time_step in range(r_out.size(1)):  # calculate output for each time step
+            outs.append(self.out(r_out[:, time_step, :]))
+        return torch.stack(outs, dim=1), h_state
+
+
 rnn = RNN()
 print(rnn)
 
-optimizer = torch.optim.Adam(rnn.parameters(), lr=LR)   # optimize all parameters
-loss_func = nn.CrossEntropyLoss()   # the target label is not one-hotted
+optimizer = torch.optim.Adam(rnn.parameters(), lr=0.001)   # optimize all parameters
+loss_func = nn.MSELoss()   # the target label is not one-hotted
 
-# training and testing
-for epoch in range(EPOCH):
-    for step, (x, b_y) in enumerate(train_loader):   # gives batch data
-        b_x = x.view(-1, 28, 28)   # reshape x to (batch, time_step, input_size)
+EPOCH = 2
+x = df_log[0][:200]
+y = df_log[0][200:]
+h_state = None
+prediction, h_state = rnn(x, h_state)
+h_state = Variable(h_state.data)
 
-        output = rnn(b_x)               # rnn output
-        loss = loss_func(output, b_y)   # cross entropy loss
-        optimizer.zero_grad()           # clear gradients for this training step
-        loss.backward()                 # backpropagation, compute gradients
-        optimizer.step()                # apply gradients
-
+loss = loss_func(prediction, y)  # cross entropy loss
+optimizer.zero_grad()  # clear gradients for this training step
+loss.backward()  # backpropagation, compute gradients
+optimizer.step()  # apply gradients
 # # Initialize model
 # net = RNN()
 #
